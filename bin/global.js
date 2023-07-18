3465d528faa3aa09e71ed6fde73e3b5e8e6cf25b
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-07-17 21:57:38 -0600

    [ADD] Reinventing the wheel. Again.

---
+++ bin/global.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node                                     
+// Es una instrucción que sirve para indicar que este archivo se ejecutará con Nodejs
+
+let random = require('../src/index.js');                // Realizamos la importación de nuestro archivo "index.js"
+
+random.funnyCommit();                                   // Ejecutamos la función "funnyCommit()"
